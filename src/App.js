99b443b4da004e440619fe2e94d3b02f440f5c6a
dylan belouis <dylan.belouis@gmail.com>
2019-11-11 01:40:17 +0100

    code api en place sans resultat

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import AxiosApi from './components/AxiosApi';
 
 function App() {
   return (
     <div className="App">
-
+      <AxiosApi />
     </div>
   );
 }
--- src/components/AxiosApi.js
+++ src/components/AxiosApi.js
@@ -1,6 +1,26 @@
 import React from 'react';
-import Axios from 'axios'; 
+import axios from 'axios'; 
 
 export default class AxiosApi extends React.Component {
-    
+    constructor(props) {
+        super(props);
+    this.state = {
+        weather: []
+    }
+    }
+
+    componentDidMount(){
+        axios.get(`https://api.meteo-concept.com/api/0?token=77ccae56f0f0a521e8f6b7b325db7bebfd3c8b7d286d4641556ebadeefd5d2f0&insee=37261`)
+        .then(res => {
+            const weather = res.data;
+            this.setState({ weather });
+        })  
+    }
+    render () {
+        return (
+            <div>
+                { this.state.weather.map(weathers => <h2>{weathers.name}</h2>)}
+            </div>
+        )
+    }
 }
