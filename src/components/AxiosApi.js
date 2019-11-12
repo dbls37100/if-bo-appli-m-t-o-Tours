8e9127312b8b38c5af936c8594587d4b61e29c4e
dylan belouis <dylan.belouis@gmail.com>
2019-11-12 22:17:41 +0100

    ajout favicon

--- src/components/AxiosApi.js
+++ src/components/AxiosApi.js
@@ -5,21 +5,32 @@ export default class AxiosApi extends React.Component {
     constructor(props) {
         super(props);
     this.state = {
-        weather: []
+        weather: null
     }
     }
-
+    
     componentDidMount(){
-        axios.get(`https://api.meteo-concept.com/api/0?token=77ccae56f0f0a521e8f6b7b325db7bebfd3c8b7d286d4641556ebadeefd5d2f0&insee=37261`)
+        axios.get("https://api.meteo-concept.com/api/forecast/daily?token=77ccae56f0f0a521e8f6b7b325db7bebfd3c8b7d286d4641556ebadeefd5d2f0&insee=37261")
         .then(res => {
-            const weather = res.data;
-            this.setState({ weather });
+            console.log(res)
+            this.setState({ weather : res.data });
         })  
     }
     render () {
         return (
-            <div>
-                { this.state.weather.map(weathers => <h2>{weathers.name}</h2>)}
+            <div> 
+                <span>Météo</span>
+                {this.state.weather &&
+                <> 
+                <h2>{this.state.weather.city.name }</h2>
+                <h2>{this.state.weather.city.cp }</h2>
+                <h2>{this.state.weather.forecast[0].datetime }</h2>
+                <h2>il fera {this.state.weather.forecast[2].tmin }° minimum.</h2>
+                <h2>il fera {this.state.weather.forecast[2].tmax }° maximum.</h2>
+                <h2>Vous pourrez profitez de {this.state.weather.forecast[1].sun_hours } heures de soleil aujourd'hui</h2>
+                </>
+                }
+
             </div>
         )
     }
