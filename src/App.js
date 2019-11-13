dd107f652e1238a667dc5924d20e115a3fc9d143
dylan belouis <dylan.belouis@gmail.com>
2019-11-13 17:47:12 +0100

    ajout gif et function pour index random dans tableau randomGifFroid

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import AxiosApi from './components/AxiosApi';
 
+
 function App() {
   return (
     <div className="App">
--- src/components/AxiosApi.js
+++ src/components/AxiosApi.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import axios from 'axios'; 
+import './AxiosApi.css';
+import './tours.jpg';
+import Froid from './giphy.GIF';
+import Froid1 from './giffroid.gif';
+import Froid2 from './gif3.gif';
+import Froid3 from './gif4.gif';
+
+const randomGifFroid =  [Froid, Froid1, Froid2, Froid3]
+
+
 
 export default class AxiosApi extends React.Component {
     constructor(props) {
         super(props);
     this.state = {
         weather: null
+        
     }
     }
+    
+    
+
     
     componentDidMount(){
         axios.get("https://api.meteo-concept.com/api/forecast/daily?token=77ccae56f0f0a521e8f6b7b325db7bebfd3c8b7d286d4641556ebadeefd5d2f0&insee=37261")
@@ -16,18 +30,40 @@ export default class AxiosApi extends React.Component {
             this.setState({ weather : res.data });
         })  
     }
+
+    gifGenerator(min, max) {
+        console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+        
+    }   
+   
+
+
+
     render () {
         return (
-            <div> 
-                <span>Météo</span>
+            <div className="AxiosApi"> 
                 {this.state.weather &&
                 <> 
-                <h2>{this.state.weather.city.name }</h2>
-                <h2>{this.state.weather.city.cp }</h2>
-                <h2>{this.state.weather.forecast[0].datetime }</h2>
-                <h2>il fera {this.state.weather.forecast[2].tmin }° minimum.</h2>
-                <h2>il fera {this.state.weather.forecast[2].tmax }° maximum.</h2>
-                <h2>Vous pourrez profitez de {this.state.weather.forecast[1].sun_hours } heures de soleil aujourd'hui</h2>
+                    <div className="render-fond">
+                        <div className="render-fond-structure">
+                            <h3>Météo</h3>
+                            <div className="image-fond--h2">
+                                <h2>{this.state.weather.city.name }</h2>
+                            </div>
+                            <h2>{this.state.weather.city.cp }</h2>
+                            <h2>Aujourd'hui :</h2>
+                            <div className="temp-align--style"> 
+                                <h2>{this.state.weather.forecast[0].tmin } ° min  |</h2>
+                                <h2>|  {this.state.weather.forecast[0].tmax } ° max</h2>
+                            </div>
+                            <h2>Vous pourrez profitez de {this.state.weather.forecast[0].sun_hours } heures de soleil aujourd'hui</h2>
+                            <div className="Gif-generator">
+                            
+                                <img src={randomGifFroid[this.gifGenerator(0,3)]} alt="GIF generator" />
+                            </div>
+                        </div>
+                    </div>
                 </>
                 }
 
